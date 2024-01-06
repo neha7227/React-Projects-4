4995e701b3eca55728107392c8327aeb9919594a
neha7227 <neha.indoliya27@gmail.com>
2024-01-06 22:29:25 +0530

    spellCheck apply created

---
+++ Spell-Check-App/spell-check-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SpellcheckApp from './components/SpellcheckApp';
+
+function App() {
+  return (
+    <div className="App">
+      <SpellcheckApp/>
+    </div>
+  );
+}
+
+export default App;
